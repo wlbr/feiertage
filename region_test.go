d702d7c6c8d5be7cb1744485ca42a1beb163df2b
Michael Wolber <mwolber@gmx.de>
2026-05-18 13:50:05 +0200

    Added license headers.

--- region_test.go
+++ region_test.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2026 Michael Wolber
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package feiertage
 
 import (
@@ -81,8 +101,8 @@ func TestFeiertageZahl(t *testing.T) {
 	checkAndFailRegionFeiertageZahl(t, Vorarlberg(2016), 14)
 	checkAndFailRegionFeiertageZahl(t, Wien(2016), 14)
 	checkAndFailRegionFeiertageZahl(t, Österreich(2016), 13)
-	checkAndFailRegionFeiertageZahl(t, All(2016), 80)
-	checkAndFailRegionFeiertageZahl(t, All(2016, false), 68)
+	checkAndFailRegionFeiertageZahl(t, All(2016), 81)
+	checkAndFailRegionFeiertageZahl(t, All(2016, false), 69)
 	//check Reformationstag 2017
 	checkAndFailRegionFeiertageZahl(t, BadenWürttemberg(2017), 13)
 	checkAndFailRegionFeiertageZahl(t, Bayern(2017), 13)
